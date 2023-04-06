00105a8ddd3f290832d443df8b9100e07363ebac
chrisgzlez <christiangv68@gmail.com>
2023-04-06 09:53:20 +0200

    Configuraciones en x86

--- Grua_Glad/source/main.cpp
+++ Grua_Glad/source/main.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 
 
+void openGlInit() {
+	//Habilito aqui el depth en vez de arriba para los usuarios de linux y mac mejor arriba
+	//Incializaciones varias
+	glClearDepth(1.0f); //Valor z-buffer
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // valor limpieza buffer color
+	glEnable(GL_DEPTH_TEST); // z-buffer
+	glEnable(GL_CULL_FACE); //ocultacion caras back
+	glCullFace(GL_BACK);
+}
+
+
+
 int main(int argc, char** argv) {
 	cout << "Esto funciona" << endl;
 }
