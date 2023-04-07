ad375ba6f88a5c64dd8a0001c7759676bfebd84e
chrisgzlez <christiangv68@gmail.com>
2023-04-07 18:25:16 +0200

    FUNCIONA + RESIZE. Version Final

--- Grua_Glad/source/main.cpp
+++ Grua_Glad/source/main.cpp
@@ -627,9 +627,11 @@ void processInput(GLFWwindow* window)
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+	w = width;
+	h = height;
 	// make sure the viewport matches the new window dimensions; note that width and 
 	// height will be significantly larger than specified on retina displays.
-	glViewport(0, 0, width, height);
+	//glViewport(0, 0, width, height);
 }
 
 void keyCallback(GLFWwindow* window, int key, int scan_code, int action, int mods) {
